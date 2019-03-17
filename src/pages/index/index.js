caeb5ca5543618533e72b5eb2afe7ce6fb56b6cb
HankinsPan <hankins.home@outlook.com>
2019-03-17 14:35:19 +0800

    toDo list

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,21 @@
 import Taro, {Component} from '@tarojs/taro'
-import {View, Text} from '@tarojs/components'
+import {
+  View,
+  Text,
+  Input,
+} from '@tarojs/components'
 import './index.scss'
 
-export default class Index extends Component {
+const toDoList = ['赵', '钱', '孙', '李', '周', '吴', '郑', '王']
+
+class Index extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      toDoList,
+      inputVal: ''
+    }
+  }
 
   config = {
     navigationBarTitleText: '首页'
@@ -23,11 +36,59 @@ export default class Index extends Component {
   componentDidHide() {
   }
 
+  inputHandler = (e) => {
+    console.log("=== inputHandler e -=-> ", e)
+    this.inputVal = e.target.value
+  }
+
+  addItem = () => {
+    const {toDoList} = this.state
+    const inputVal = this.inputVal
+
+    console.log("==== toDoList -=--> ", toDoList)
+
+    if (!inputVal) return
+
+    toDoList.push(inputVal)
+    this.setState({
+      toDoList,
+      inputVal: ''
+    })
+  }
+
+  delItem = (index) => {
+    const {toDoList} = this.state
+    toDoList.splice(index, 1)
+    this.setState({
+      toDoList
+    })
+  }
+
   render() {
+    const {toDoList, inputVal} = this.state
+    console.log("=== this.state -=--> ", this.state)
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <Input
+          className='input'
+          type='text'
+          value={inputVal}
+          onInput={this.inputHandler.bind(this)}
+        />
+        <Text className='add' onClick={this.addItem.bind(this)}>添加</Text>
+        {
+          toDoList.map((item, index) => {
+            return (
+              <View key={index}>
+                <Text>{item}</Text>
+                <Text className='del' onClick={this.delItem.bind(this, index)}>删除</Text>
+              </View>
+            )
+          })
+        }
       </View>
     )
   }
 }
+
+export default Index
